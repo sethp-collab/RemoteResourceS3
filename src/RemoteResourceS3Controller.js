edf8b3fa88dea86c0601f03d130cddf388f3e77b
Adam King <rak@linux.vnet.ibm.com>
2019-09-27 12:44:15 -0400

    DNM: Initial code to get all the objects from an S3 bucket with a specific… (#18)
    * Initial code to get all the objects from an S3 bucket with a specific prefix
    
    * Update RemoteResourceS3Controller.js
    
    * Initially functional bucket get
    
    * Add error statuscode path. Handle uri key as url
    
    * Address PR review comments
    
    * Ignore download-cache when its created while running locally
    
    * Update RemoteResourceS3Controller.js
    
    * Handle URL without a bucket name
    
    * Update RemoteResourceS3Controller.js
    
    * Eliminate duplicate code

--- src/RemoteResourceS3Controller.js
+++ src/RemoteResourceS3Controller.js
@@ -17,14 +17,96 @@
 const objectPath = require('object-path');
 const request = require('request-promise-native');
 const merge = require('deepmerge');
-const log = require('./bunyan-api').createLogger('RemoteResourceS3Controller');
+const xml2js = require('xml2js');
+const clone = require('clone');
+const loggerFactory = require('./bunyan-api');
 const { BaseDownloadController } = require('@razee/razeedeploy-core');
 
 
 module.exports = class RemoteResourceS3Controller extends BaseDownloadController {
   constructor(params) {
     params.finalizerString = params.finalizerString || 'children.downloads.deploy.razee.io';
+    params.logger = params.logger || loggerFactory.createLogger('RemoteResourceS3Controller');
     super(params);
+  }
+
+  async _fixUrl(url) {
+    const u = new URL(url);
+    if (u.hostname.startsWith('s3.')) { //The bucket name is part of the path
+      let pathSegments = u.pathname.split('/');
+      pathSegments.shift(); //Discard the leading slash
+      u.pathname = pathSegments.shift();
+      u.search = `prefix=${pathSegments.join('/')}`;
+    } else { //The bucket name is part of the hostname
+      let hostnameSegments = u.hostname.split('.');
+      let bucket = hostnameSegments.shift();
+      u.hostname = hostnameSegments.join('.');
+      let prefix = u.pathname.slice(1, u.pathname.length);
+      u.search = `prefix=${prefix}`;
+      u.pathname = bucket;
+    }
+    if (u.pathname === '/') {
+      this.log.error(`No bucket name found for ${url}`);
+      return Promise.reject({ statusCode: 500, uri: url, message: `Error getting bucket name from ${url}` });
+    }
+    return u.toString();
+  }
+
+  async _getBucketObjectRequestList(bucketRequest) {
+    const result = [];
+    const bucketUrl = objectPath.get(bucketRequest, 'options.url', objectPath.get(bucketRequest, 'options.uri'));
+    objectPath.del(bucketRequest, 'options.uri');
+    const url = await this._fixUrl(bucketUrl);
+    objectPath.set(bucketRequest, 'options.url', url);
+    const objectListResponse = await this.download(bucketRequest.options);
+    if (objectListResponse.statusCode >= 200 && objectListResponse.statusCode < 300) {
+      this.log.debug(`Download ${objectListResponse.statusCode} ${url}`);
+      const objectListString = objectListResponse.body;
+      const parser = new xml2js.Parser();
+      try {
+        const objectList = await parser.parseStringPromise(objectListString);
+        const xmlns = objectPath.get(objectList, 'ListBucketResult.$.xmlns');
+        if (xmlns !== 'http://s3.amazonaws.com/doc/2006-03-01/') {
+          this.log.warn(`Unexpected S3 bucket object list namespace of ${xmlns}.`);
+        }
+        let bucket = objectPath.get(objectList, 'ListBucketResult.Name');
+        let objectsArray = objectPath.get(objectList, 'ListBucketResult.Contents', []);
+        objectsArray.forEach((o) => {
+          const objectKey = objectPath.get(o, 'Key.0');
+          const reqClone = clone(bucketRequest);
+          const newUrl = new URL(url);
+          newUrl.pathname = `${bucket}/${objectKey}`;
+          newUrl.searchParams.delete('prefix');
+          objectPath.set(reqClone, 'options.url', newUrl.toString());
+          result.push(reqClone);
+        });
+      } catch (err) {
+        this.log.error(err, `Error getting bucket listing for ${url}`);
+        return Promise.reject({ statusCode: 500, uri: url, message: `Error getting bucket listing for ${url}` });
+      }
+    } else {
+      this.log.error(`Download failed: ${objectListResponse.statusCode} | ${url}`);
+      return Promise.reject({ statusCode: objectListResponse.statusCode, uri: url });
+    }
+    return result;
+  }
+
+  async added() {
+    let requests = objectPath.get(this.data, ['object', 'spec', 'requests'], []);
+    let newRequests = [];
+    for (let i = 0; i < requests.length; i++) {
+      let r = requests[i];
+      const url = new URL(objectPath.get(r, 'options.url'));
+      if (url.pathname.endsWith('/')) { //This is an S3 bucket
+        let additionalRequests = await this._getBucketObjectRequestList(r);
+        newRequests = newRequests.concat(additionalRequests);
+      } else {
+        newRequests.push(r);
+      }
+    }
+    objectPath.set(this.data, ['object', 'spec', 'requests'], newRequests);
+    let result = await super.added();
+    return result;
   }
 
   async download(reqOpt) {
@@ -51,7 +133,7 @@ module.exports = class RemoteResourceS3Controller extends BaseDownloadController
       objectPath.set(options, 'headers.Authorization', `bearer ${bearerToken}`);
     }
     let opt = merge(reqOpt, options);
-    log.debug(`Download ${opt.uri || opt.url}`);
+    this.log.debug(`Download ${opt.uri || opt.url}`);
 
     opt.simple = false;
     opt.resolveWithFullResponse = true;
